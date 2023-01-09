7fb9d5b18cf1edeab5d861d1320766010473cc08
SaNjaYsinGh1123 <sanjayfbd93544@gmail.com>
2023-01-09 11:13:23 +0530

    commit 2

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+
+const router = express.Router();
+
+app.get('/',function(req,res){
+     return res.send('<h1>hi you are on demo page</h1>');
+});
+
+
+
 app.listen(process.env.PORT ||8000, function(err){
     if(err){
         console.log('error:',err);
